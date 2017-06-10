d4590d51aaa0b90760cd5d5cacfac565f13d3a8b
Daniel Bengtsson <daniel@bengtssons.info>
2017-06-10 12:41:47 +0200

    Fruits

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 extern crate pancurses;
+extern crate rand;
 
-use pancurses::{initscr, curs_set, endwin, Input, noecho};
+use pancurses::*;
+use rand::distributions::{IndependentSample, Range};
 use std::{thread, time};
 use std::collections::VecDeque;
 
@@ -12,7 +14,7 @@ enum Direction {
     Still,
 }
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq)]
 struct Pos {
     x: i32,
     y: i32,
@@ -32,14 +34,15 @@ fn main() {
     // Colors
     init_pair(1, COLOR_GREEN, COLOR_BLACK);
     init_pair(2, COLOR_RED, COLOR_BLACK);
+    init_pair(3, COLOR_YELLOW, COLOR_BLACK);
 
-    win.attrset(ColorPair(1));
     win.nodelay(true); // Makes getch() non-blocking
+    let max = win.get_max_yx();
 
     let mut snake = Snake {
         p: VecDeque::new(),
         d: Direction::Still,
-        l: 10
+        l: 3
     };
     snake.p.push_front(Pos { x: 0, y: 0 });
 
@@ -47,11 +50,23 @@ fn main() {
     curs_set(0);
     noecho();
 
+    // Add some fruits
+    let mut fruits = Vec::new();
+    win.attrset(ColorPair(3));
+    let mut rng = rand::thread_rng();
+    for _ in 0..5 {
+        let y = Range::new(0, max.0).ind_sample(&mut rng);
+        let x = Range::new(0, max.1).ind_sample(&mut rng);
+        fruits.push(Pos{x: x, y: y});
+        win.mvaddch(y, x, '¤');
+    }
+    win.attrset(ColorPair(1));
+
     loop {
         let head = snake.p.front().unwrap().clone();
         win.mvaddch(head.y, head.x, '@');
 
-        thread::sleep(time::Duration::from_millis(50));
+        thread::sleep(time::Duration::from_millis(100));
         match win.getch() {
             Some(k) => {
                 match k {
@@ -79,7 +94,6 @@ fn main() {
         }
 
         // Collision check
-        let max = win.get_max_yx();
         let new_head = snake.p.front().unwrap().clone();
         if new_head.y < 0 || new_head.x < 0 || new_head.y > max.0 || new_head.x > max.1 {
             snake.d = Direction::Still;
@@ -88,6 +102,29 @@ fn main() {
                 win.mvaddch(p.y, p.x, 'X');
             }
             win.attrset(ColorPair(1));
+        }
+        let mut eaten = None;
+        for (i, fruit) in fruits.iter().enumerate() {
+            if *fruit == new_head {
+                snake.l += 2;
+                eaten = Some(i);
+                break;
+            }
+        }
+        match eaten {
+            Some(i) => {
+                let _ = fruits.remove(i);
+                // Add new fruits
+                // TODO: Make sure it's in an empty spot
+                win.attrset(ColorPair(3));
+                let mut rng = rand::thread_rng();
+                let y = Range::new(0, max.0).ind_sample(&mut rng);
+                let x = Range::new(0, max.1).ind_sample(&mut rng);
+                fruits.push(Pos{x: x, y: y});
+                win.mvaddch(y, x, '¤');
+                win.attrset(ColorPair(1));
+            },
+            None => {}
         }
     }
     endwin();
