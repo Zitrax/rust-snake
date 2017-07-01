1c8b1e27c628b181376c11e14f3cb44bb974cbf5
Daniel Bengtsson <daniel@bengtssons.info>
2017-07-02 01:06:12 +0200

    Refactored a bit

--- src/main.rs
+++ src/main.rs
@@ -22,9 +22,10 @@ struct Pos {
 }
 
 struct Snake {
-    p: VecDeque<Pos>,
+    p: VecDeque<Pos>, // positions
     d: Direction,
-    l: usize,
+    l: usize,         // length
+    c: u8             // color id
 }
 
 impl Snake {
@@ -73,7 +74,9 @@ impl Snake {
 
     // Move snake according to direction
     fn mv(&mut self, win: &Window) {
+        win.attrset(ColorPair(self.c));
         let head = self.p.front().unwrap().clone();
+        win.mvaddch(head.y, head.x, '@');
         match self.d {
             Direction::Up => self.p.push_front(Pos { x: head.x, y: head.y - 1 }),
             Direction::Down => self.p.push_front(Pos { x: head.x, y: head.y + 1 }),
@@ -84,6 +87,41 @@ impl Snake {
         if self.p.len() > self.l {
             let back = self.p.pop_back().unwrap();
             win.mvaddch(back.y, back.x, ' ');
+        }
+    }
+
+    // Collision checks
+    fn collision(&mut self, win: &Window, fruits: &mut Vec<Pos>, fruitsymbol: char) {
+        let max = win.get_max_yx();
+        let new_head = self.p.front().unwrap().clone();
+        if new_head.y < 0 || new_head.x < 0 || new_head.y > max.0 || new_head.x > max.1 {
+            self.d = Direction::Still;
+            win.attrset(ColorPair(2));
+            for p in self.p.iter() {
+                win.mvaddch(p.y, p.x, 'X');
+            }
+        }
+        let mut eaten = None;
+        for (i, fruit) in fruits.iter().enumerate() {
+            if *fruit == new_head {
+                self.l += 2;
+                eaten = Some(i);
+                break;
+            }
+        }
+        match eaten {
+            Some(i) => {
+                let _ = fruits.remove(i);
+                // Add new fruits
+                // TODO: Make sure it's in an empty spot
+                win.attrset(ColorPair(3));
+                let mut rng = rand::thread_rng();
+                let y = Range::new(0, max.0).ind_sample(&mut rng);
+                let x = Range::new(0, max.1).ind_sample(&mut rng);
+                fruits.push(Pos { x: x, y: y });
+                win.mvaddch(y, x, fruitsymbol);
+            }
+            None => {}
         }
     }
 }
@@ -106,6 +144,7 @@ fn main() {
         p: VecDeque::new(),
         d: Direction::Still,
         l: 3,
+        c: 1
     };
     snake.p.push_front(Pos { x: max.1 / 2, y: max.0 / 2 });
 
@@ -124,13 +163,8 @@ fn main() {
         fruits.push(Pos { x: x, y: y });
         win.mvaddch(y, x, fruitsymbol);
     }
-    win.attrset(ColorPair(1));
 
     loop {
-        let head = snake.p.front().unwrap().clone();
-        win.mvaddch(head.y, head.x, '@');
-
-        thread::sleep(time::Duration::from_millis(100));
         // Read key and take action
         match win.getch() {
             Some(k) => {
@@ -142,40 +176,8 @@ fn main() {
             None => (),
         }
         snake.mv(&win);
-
-        // Collision check
-        let new_head = snake.p.front().unwrap().clone();
-        if new_head.y < 0 || new_head.x < 0 || new_head.y > max.0 || new_head.x > max.1 {
-            snake.d = Direction::Still;
-            win.attrset(ColorPair(2));
-            for p in snake.p.iter() {
-                win.mvaddch(p.y, p.x, 'X');
-            }
-            win.attrset(ColorPair(1));
-        }
-        let mut eaten = None;
-        for (i, fruit) in fruits.iter().enumerate() {
-            if *fruit == new_head {
-                snake.l += 2;
-                eaten = Some(i);
-                break;
-            }
-        }
-        match eaten {
-            Some(i) => {
-                let _ = fruits.remove(i);
-                // Add new fruits
-                // TODO: Make sure it's in an empty spot
-                win.attrset(ColorPair(3));
-                let mut rng = rand::thread_rng();
-                let y = Range::new(0, max.0).ind_sample(&mut rng);
-                let x = Range::new(0, max.1).ind_sample(&mut rng);
-                fruits.push(Pos { x: x, y: y });
-                win.mvaddch(y, x, fruitsymbol);
-                win.attrset(ColorPair(1));
-            }
-            None => {}
-        }
+        snake.collision(&win, &mut fruits, fruitsymbol);
+        thread::sleep(time::Duration::from_millis(100));
     }
     endwin();
 }
